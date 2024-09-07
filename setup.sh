c0aef9e3f62461a3c720623d74a1b10b8743c37e
Kazi Hossain <keh8423@nyu.edu>
2024-09-07 18:11:59 -0400

    git skill issues

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd homeworks
+git remote add upstream https://github.com/remzi-arpacidusseau/ostep-homework
+git remote -v
+git fetch upstream
+git checkout -b main upstream/main
+cd ..
+cd projects 
+git remote add upstream https://github.com/remzi-arpacidusseau/ostep-projects 
+git remote -v
+git fetch upstream
+git checkout -b main upstream/main
