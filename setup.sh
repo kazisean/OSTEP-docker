773f89d49480b18c3115070fc0c937c4198067da
Kazi Hossain <keh8423@nyu.edu>
2024-09-07 18:14:45 -0400

    I am new to bash

--- setup.sh
+++ setup.sh
@@ -4,10 +4,10 @@ cd homeworks
 git remote add upstream https://github.com/remzi-arpacidusseau/ostep-homework
 git remote -v
 git fetch upstream
-git checkout -b main upstream/main
+git checkout -b main upstream/master
 cd ..
 cd projects 
 git remote add upstream https://github.com/remzi-arpacidusseau/ostep-projects 
 git remote -v
 git fetch upstream
-git checkout -b main upstream/main
+git checkout -b main upstream/master
